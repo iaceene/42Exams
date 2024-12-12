7fd4d67125c6864cf2951f20749294145a2e934a
Yassine Ajagrou <106110604+iaceene@users.noreply.github.com>
2024-12-12 01:38:18 +0100

    exam

--- rev_wstr/rev_wstr.c
+++ rev_wstr/rev_wstr.c
@@ -1,64 +1,31 @@
 #include "unistd.h"
 
-int is(char c)
+void rev_wstr(char *s)
 {
-    return (c == ' ' || c == '\n' || c == '\t');
-}
-
-int ft_countwords(char *s)
-{
-    int i  = 0;
-    int count = 0;
-    while(s[i])
+    int len = -1;
+    while(s[++len] == ' ');
+    s = s + len;
+    len = 0;
+    while(s[++len]);
+    int start, end = 0;
+    while(--len && len >= 0)
     {
-        while(s[i] && is(s[i]))
-            i++;
-        if(s[i])
-            count++;
-        while (s[i] && !is(s[i]))
-            i++;
-    }
-    return (count);
-}
-
-void ft_print(char *s, int start, int end, int *count)
-{
-    if (start == -1)
-        start = 0;
-    while(start <= end)
-    {
-        write(1, &s[start], 1);
-        start++;
-    }
-    (*count)++;
-    if(*count == ft_countwords(s) - 1)
-        write(1, " ", 1);
-}
-
-void ft(char *s)
-{
-    int count = 0;
-    int end = 0;
-    int i = 0;
-    while (s[i])
-        i++;
-    i--;
-    while(s[i])
-    {
-        while(s[i] && is(s[i]))
-            i--;
-        if(s[i])
-            end = i;
-        while(s[i] && !is(s[i]))
-            i--;
-        ft_print(s, i, end, &count);
+        while(*(s + len) && *(s + len) == ' ')
+            len--;
+        end = len + 1;
+        while(*(s + len) && *(s + len) != ' ')
+            len--;
+        start = len;
+        while(++start < end)
+            write(1, s + start, 1);
+        if(len - 1 > 0)
+            write(1, " ", 1);
     }
 }
 
 int main(int c, char **v)
 {
     if(c == 2)
-        ft(v[1]);
-    write(1, "\n", 1);
-    return (0);
+        rev_wstr(v[1]);
+    return(write(1, "\n", 1), 0);
 }
